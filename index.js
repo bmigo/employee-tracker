6bffc58131021e46b504b7367fc500e0933a81b1
bmigo <berniecmigo@gmail.com>
2023-01-31 17:52:04 -0500

    added mysql connection

---
+++ index.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        user: 'root',
+        password: '',
+        database: 'employee_db'
+    }
+    
+);
+
